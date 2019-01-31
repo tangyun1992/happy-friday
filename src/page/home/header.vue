4621b2ff47ef455785058a795339a0af1d767684
tangyun <tangyun@360.net>
2019-01-31 12:54:45 +0800

    header布局

---
+++ src/page/home/header.vue
@@ -0,0 +1,74 @@
+
+<template>
+  <div class="main-header">
+    <div class="left">
+      <span class="logo"><i class="el-icon-tickets" style="color: #ddd;"></i>中国电信</span>
+      <span class="title"><i class="el-icon-arrow-right"></i>电信政企协议管理系统</span>
+    </div>
+    <div class="right">
+      <span>{{name}} 欢迎您</span>
+      <el-button type="danger" @click="exit" class="exit">安全退出</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'main-header',
+    components: {},
+    data () {
+      return {
+        name: 'admin'
+      }
+    },
+    props: {},
+    watch: {},
+    methods: {
+      exit () {
+
+      }
+    },
+    mounted () {
+    }
+  }
+</script>
+
+<style scoped lang="less">
+.main-header {
+  background-color: rgb(69, 103, 130);
+  height: 60px;
+  color: #fff;
+  line-height: 60px;
+  .left {
+    display:inline-block;
+    .logo {
+      font-size: 25px;
+      font-family: 华文楷体;
+      margin: 0 40px 0 20px;
+    }
+    .title {
+      font-weight: bold;
+      font-size: 18px;
+      letter-spacing: 2px;
+    }
+  }
+  .right {
+    margin-right: 20px;
+    display: inline-block;
+    float: right;
+    .exit{
+      margin-left: 20px;
+    }
+  }
+}
+</style>
+<style lang="less">
+  .main-header{
+    .el-button {
+      background-color: red;
+      padding: 5px;
+      border-radius: 2px;
+      border-color:red;
+    }
+  }
+</style>
--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -1,85 +1,44 @@
 <template>
-  <div>
-    <el-row class="tac">
-      <el-col :span="12">
-        <h5>默认颜色</h5>
+  <div class="home">
+    <main-header></main-header>
+    <el-row class="menu" >
+      <el-col >
         <el-menu
+          background-color="rgb(101, 149, 188)"
+          text-color="#fff"
           default-active="2"
-          class="el-menu-vertical-demo"
           @open="handleOpen"
           @close="handleClose">
-          <el-submenu index="1">
+          <el-menu-item index="1">
             <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
+              <i class="el-icon-menu my-icon"></i>
+              <span>主页</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
+          </el-menu-item>
+          <el-submenu index="2">
+            <span slot="title">系统管理</span>
           </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
-          </el-menu-item>
-        </el-menu>
-      </el-col>
-      <el-col :span="12">
-        <h5>自定义颜色</h5>
-        <el-menu
-          default-active="2"
-          class="el-menu-vertical-demo"
-          @open="handleOpen"
-          @close="handleClose"
-          background-color="#545c64"
-          text-color="#fff"
-          active-text-color="#ffd04b">
-          <el-submenu index="1">
+          <el-submenu index="3">
+            <span slot="title">模板管理</span>
+          </el-submenu>
+          <el-submenu index="4">
+            <span slot="title">我的流程</span>
+          </el-submenu>
+          <el-submenu index="4">
+            <span slot="title">客户列表</span>
+          </el-submenu>
+          <el-submenu index="4">
             <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
+            <i class="el-icon-setting my-icon"></i>
+            <span slot="title">统计报表</span>
             </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
           </el-submenu>
-          <el-menu-item index="2">
-            <i class="el-icon-menu"></i>
-            <span slot="title">导航二</span>
-          </el-menu-item>
-          <el-menu-item index="3" disabled>
-            <i class="el-icon-document"></i>
-            <span slot="title">导航三</span>
-          </el-menu-item>
-          <el-menu-item index="4">
-            <i class="el-icon-setting"></i>
-            <span slot="title">导航四</span>
-          </el-menu-item>
+          <el-submenu index="4">
+            <template slot="title">
+            <i class="el-icon-setting my-icon"></i>
+            <span slot="title">基础信息管理</span>
+            </template>
+          </el-submenu>
         </el-menu>
       </el-col>
     </el-row>
@@ -88,9 +47,10 @@
 </template>
 
 <script>
+  import mainHeader from './header'
   export default {
     name: 'index',
-    components: {},
+    components: {mainHeader},
     data () {
       return {}
     },
@@ -109,6 +69,23 @@
   }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+  .my-icon {
+    color: #fff;
+  }
+  .menu {
+    width: 200px;
+  }
 
+</style>
+<style lang="less">
+  .home {
+    .el-menu {
+      height: 800px;
+    }
+    .el-menu-item [class^=el-icon-], .el-submenu [class^=el-icon-] {
+      margin-right: 0;
+      width: auto;
+    }
+  }
 </style>
